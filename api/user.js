2150900ceff75a04d55c787dd564ef4fbf84d789
GeovaniBueno1972 <you@example.comgeovani.bueno@yahoo.com.br>
2021-10-04 22:38:06 -0300

    teste banco

--- api/user.js
+++ api/user.js
@@ -1,8 +1,28 @@
-const bcrypt = require('bcrypt-nodejs')
+const express = require('express');
+const { Pool } = require('pg');
+
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 
 module.exports = app =>{
-
+    express()
+        .get('/usuarios',  async (req, res) => {
+        try {
+          const client = await pool.connect();
+          const result = await client.query('SELECT * FROM usuarios');
+          const results = { 'results': (result) ? result.rows : null};
+          client.release();
+        } catch (err) {
+          console.error(err);
+          res.send("Error " + err);
+        }
+      })
     
 
     const save = async (req, res) => {
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,6 @@
 module.exports = app => {
     app.route('/users')
         .post(app.api.user.save)
+        
+    
 }
